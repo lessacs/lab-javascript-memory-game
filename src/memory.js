5a5e4939c66c47835941a936d35cb92c11d2e44b
Arcticc <30600726+lessacs@users.noreply.github.com>
2023-01-27 11:48:30 +0100

    lab done

--- src/memory.js
+++ src/memory.js
@@ -1,18 +1,41 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
-    // ... write your code here
+    if (!this.cards) {
+      return undefined;
+    }
+
+    for (let i = 0; i < this.cards.length; i++) {
+    let shuffle = Math.floor(Math.random() * (this.cards.length));
+    let temp = this.cards[i];
+    this.cards[i] = this.cards[shuffle];
+    this.cards[shuffle] = temp;
+    return this.cards;
   }
+}
 
   checkIfPair(card1, card2) {
-    // ... write your code here
-  }
-
+    this.pairsClicked++;
+        if (card1 === card2) {
+          this.pairsGuessed++;
+            return true;
+          } else {
+            return false;
+          }
+      }
+    
   checkIfFinished() {
-    // ... write your code here
+    if (this.pairsGuessed === this.cards.length) {
+      return 'game finished';
+    } else {
+    return false;
   }
+}
 }
